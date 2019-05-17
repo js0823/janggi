34bd942de4867661396405b4affccefac6485b1b
Jongseo Yoon <js0823@gmail.com>
2019-05-16 22:15:24 -0400

    working with pygame

--- janggi.py
+++ janggi.py
@@ -3,3 +3,22 @@
 # Author : Jongseo Yoon
 
 import pygame
+from pygame.locals import *
+
+def main():
+    pygame.init()
+    logo = pygame.image.load('logo.png')
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption('Janggi')
+
+    screen = pygame.display.set_mode((800, 600))
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+if __name__ == '__main__':
+    main()
